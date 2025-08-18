5668d34542651df1258a98fcf5b89867e8e06f3a
А Н <dim.ni@bk.ru>
2025-08-18 17:21:43 +0300

    formated2

--- Computer.java
+++ Computer.java
@@ -1,12 +1,10 @@
 import static java.lang.Math.*;
 
 public class Computer {
-
     public static final int COLUMN = 12;
     public static final int ROW = 15;
 
     public static void main(String[] args) {
-
         int n[] = {3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25};
         double x[] = new double[ROW];
         double w[][] = new double[COLUMN][ROW];
@@ -29,22 +27,18 @@ public class Computer {
     }
 
     public static double degreeOfArctangent(double x) {
-
         return pow(atan(0.2 * (x + 1) / 14.0), 0.25 / asin(pow(E, - abs(x))) - 1);
     }
 
     public static double cosOfCos(double x) {
-
         return cos(cos(cbrt(x)));
     }
 
     public static double degreeOfExpression(double x) {
-
         return pow(0.25 - pow(cbrt(pow(x, x - 3.0 / 4.0)), 3 + cos(cbrt(x))), asin(pow(E, cbrt(pow( - (4 / abs(x)), x)))));
     }
 
     public static void printMatrix(double[][] twoDimentionalMassive) {
-
         for (int i = 0; i < twoDimentionalMassive.length; ++i) {
             for (int j = 0; j < twoDimentionalMassive[0].length; ++j) {
                 System.out.printf("%-10.4f", twoDimentionalMassive[i][j]);
