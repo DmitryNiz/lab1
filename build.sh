5dc355aaa93d3b2d26ffdceef4c3febd34c480c3
А Н <dim.ni@bk.ru>
2025-08-25 19:52:55 +0300

    delete manifest

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 mkdir -p jararch
 javac -d jararch *.java
-echo "Main-Class: Computer" > manifest.txt
-jar cfm Computer.jar manifest.txt -C jararch .
-rm manifest.txt
+jar cef Computer Computer.jar -C jararch .
+
 
